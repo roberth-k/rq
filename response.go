2c35b44698f3b6cdbf69b88417869f02aa728f87
Roberth Kulbin <roberth.kulbin@magairports.com>
2019-07-25 11:12:37 +0100

    rq: add Is2xx() etc. helpers to response

--- response.go
+++ response.go
@@ -30,3 +30,23 @@ func (resp *Response) Unmarshal(v interface{}) error {
 
 	return json.Unmarshal(data, v)
 }
+
+func (resp *Response) Is1xx() bool {
+	return resp.Status >= 100 && resp.Status < 200
+}
+
+func (resp *Response) Is2xx() bool {
+	return resp.Status >= 200 && resp.Status < 300
+}
+
+func (resp *Response) Is3xx() bool {
+	return resp.Status >= 300 && resp.Status < 400
+}
+
+func (resp *Response) Is4xx() bool {
+	return resp.Status >= 400 && resp.Status < 500
+}
+
+func (resp *Response) Is5xx() bool {
+	return resp.Status >= 500 && resp.Status < 600
+}
