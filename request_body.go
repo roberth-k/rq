75b361aa7f6f87369fe3e6a80e2211248b0c2487
Roberth Kulbin <roberth@winter-weht.net>
2019-07-27 20:30:03 +0100

    rq: unit tests for all body methods

--- request_body.go
+++ request_body.go
@@ -2,24 +2,44 @@ package rq
 
 import (
 	"bytes"
+	"io"
+	"strings"
 )
 
-func (req Request) Marshal(v interface{}) Request {
-	req1, err := req.getMarshallerOrDefault()(req, v)
-	if err != nil {
-		req.err = err
-		return req
-	}
-
-	return req1
+func (req Request) SetMarshaller(marshaller Marshaller) Request {
+	req.Marshaller = marshaller
+	return req
 }
 
-func (req Request) WithBodyAsBytes(data []byte) Request {
+func (req Request) SetBody(reader io.Reader) Request {
+	req.Body = reader
+	return req
+}
+
+func (req Request) SetBodyBytes(data []byte) Request {
 	req.Body = bytes.NewReader(data)
 	return req
 }
 
-func (req Request) WithBodyFromJSON(v interface{}) Request {
+func (req Request) SetBodyString(data string) Request {
+	req.Body = strings.NewReader(data)
+	return req
+}
+
+// Marshal sets the body of the request by marshalling the given value using
+// the Marshaller set on the request object. If none is set, it is marshalled
+// using the MarshalJSON() method of the request.
+func (req Request) Marshal(value interface{}) Request {
+	if req.Marshaller == nil {
+		return req.MarshalJSON(value)
+	}
+
+	return req.Marshaller(req, value)
+}
+
+// MarshalJSON marshals the given value using the default JSON marshaller.
+// The Content-Type of the request will be set to "application/json; charset=utf-8".
+func (req Request) MarshalJSON(value interface{}) Request {
 	req.Marshaller = JSONMarshaller
-	return req.Marshal(v)
+	return req.Marshal(value)
 }
--- request_body_test.go
+++ request_body_test.go
@@ -3,9 +3,99 @@ package rq_test
 import (
 	"context"
 	"github.com/stretchr/testify/require"
+	"github.com/tetratom/rq"
+	"io/ioutil"
 	"net/http"
+	"strings"
 	"testing"
 )
+
+func TestRequest_SetMarshaller(t *testing.T) {
+	t.Parallel()
+
+	req := rq.Request{}
+	require.Nil(t, req.Marshaller)
+
+	m := func(req rq.Request, _ interface{}) rq.Request {
+		return req
+	}
+
+	req = req.SetMarshaller(m)
+	require.NotNil(t, req.Marshaller)
+}
+
+func TestRequest_SetBody(t *testing.T) {
+	t.Parallel()
+	req := rq.Request{}
+	require.Nil(t, req.Body)
+	r := strings.NewReader("test")
+	req = req.SetBody(r)
+	require.Equal(t, r, req.Body)
+}
+
+func TestRequest_SetBodyBytes(t *testing.T) {
+	t.Parallel()
+	req := rq.Request{}
+	require.Nil(t, req.Body)
+	expect := []byte("test")
+	req = req.SetBodyBytes(expect)
+	require.NotNil(t, req.Body)
+	actual, err := ioutil.ReadAll(req.Body)
+	require.NoError(t, err)
+	require.Equal(t, expect, actual)
+}
+
+func TestRequest_SetBodyString(t *testing.T) {
+	t.Parallel()
+	req := rq.Request{}
+	require.Nil(t, req.Body)
+	expect := "test"
+	req = req.SetBodyString(expect)
+	require.NotNil(t, req.Body)
+	actual, err := ioutil.ReadAll(req.Body)
+	require.NoError(t, err)
+	require.Equal(t, expect, string(actual))
+}
+
+func TestRequest_Marshal(t *testing.T) {
+	t.Parallel()
+
+	var calls int
+	var value interface{}
+
+	m := func(req rq.Request, v interface{}) rq.Request {
+		calls++
+		value = v
+
+		return req.SetHeader("Test", "done")
+	}
+
+	req := rq.Begin()
+	require.False(t, req.HasHeader("Test"))
+
+	req = req.SetMarshaller(m)
+	require.Zero(t, calls)
+
+	actual := map[string]string{"x": "y"}
+
+	req = req.Marshal(actual)
+	require.Nil(t, req.Body)
+	require.Equal(t, "done", req.GetHeader("Test"))
+	require.Equal(t, 1, calls)
+	require.Equal(t, actual, value)
+}
+
+func TestRequest_MarshalJSON(t *testing.T) {
+	t.Parallel()
+
+	actual := map[string]string{"x": "y"}
+	req := rq.Begin().MarshalJSON(actual)
+	require.NotNil(t, req.Body)
+	data, err := ioutil.ReadAll(req.Body)
+	require.NoError(t, err)
+	require.Equal(t, `{"x":"y"}`, string(data))
+	require.Equal(t, "application/json; charset=utf-8", req.GetHeader("Content-Type"))
+}
 
 func TestBasicMarshalling(t *testing.T) {
 	t.Parallel()
@@ -16,7 +106,7 @@ func TestBasicMarshalling(t *testing.T) {
 
 	ctx := context.Background()
 	input := Test{Foo: 42}
-	req := HTTPBin().JoinURL("/anything").WithBodyFromJSON(input)
+	req := HTTPBin().JoinURL("/anything").MarshalJSON(input)
 	require.Equal(t, http.Header{"Content-Type": []string{"application/json; charset=utf-8"}}, req.HeaderMap())
 	rep, err := req.POST(ctx)
 	require.NoError(t, err)
