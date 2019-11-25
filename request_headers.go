c453c296cc5988927773e6d05460a56955c8a3c3
Roberth Kulbin <roberth@winter-weht.net>
2019-11-25 20:13:26 +0000

    rq: fix request header immutability

--- request_headers.go
+++ request_headers.go
@@ -19,15 +19,23 @@ func (req Request) HeaderMap() http.Header {
 	return m
 }
 
-func (req Request) AddHeader(name string, value string, args ...interface{}) Request {
-	name = textproto.CanonicalMIMEHeaderKey(name)
-
-	if len(args) > 0 {
-		value = fmt.Sprintf(value, args...)
-	}
-
-	req.Headers = append(req.Headers, Header{Name: name, Value: value})
+func (req Request) MapHeaders(f func([]Header) []Header) Request {
+	out := make([]Header, len(req.Headers), len(req.Headers)+2)
+	copy(out, req.Headers)
+	req.Headers = f(out)
 	return req
+}
+
+func (req Request) AddHeader(name string, value string, args ...interface{}) Request {
+	return req.MapHeaders(func(headers []Header) []Header {
+		name = textproto.CanonicalMIMEHeaderKey(name)
+
+		if len(args) > 0 {
+			value = fmt.Sprintf(value, args...)
+		}
+
+		return append(headers, Header{Name: name, Value: value})
+	})
 }
 
 func (req Request) GetHeader(name string) string {
